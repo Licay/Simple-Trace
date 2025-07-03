f076e09421f6b42df0cd4f206db3426f4f188e88
Licay <40955819+Licay@users.noreply.github.com>
2025-07-03 15:40:47 +0800

    [fix] tools/setup.sh use sed to patch

--- tools/setup.sh
+++ tools/setup.sh
@@ -14,7 +14,9 @@ fi
 git clone https://github.com/Licay/Simple-Trace.git --branch $BRANCH $KERNEL_DIR/drivers/simple_trace
 
 cd $KERNEL_DIR
-git apply $KERNEL_DIR/drivers/simple_trace/tools/kernel.diff
+# git apply $KERNEL_DIR/drivers/simple_trace/tools/kernel.diff
+sed -i '0,/^$/s//source "drivers\/simple_trace\/Kconfig"\n/' $KERNEL_DIR/drivers/Kconfig
+sed -i '0,/^$/s//obj-y\t\t\t\t+= simple_trace\/\n/' $KERNEL_DIR/drivers/Makefile
 cd -
 
 echo "Done! Kernel source is ready for Simple Trace."
